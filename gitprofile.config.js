5b519901ebeef00474f0c4734b7ec0799fe70e77
Sumanth, Tatipamula <sumanthtatipamula999@gmail.com>
2022-12-11 12:05:05 +0530

    updates path

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -6,7 +6,7 @@ const config = {
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
+      forks: true, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
@@ -93,7 +93,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'autumn',
+    defaultTheme: 'luxury',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
